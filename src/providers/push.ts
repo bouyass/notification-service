6dbd510c0204b7b0ea875f78a8df9ab5a7dcdd9f
lyes makhloufi <lyes.makhloufi@outlook.fr>
2025-08-17 17:17:46 +0200

    decodeing secret env

--- src/providers/push.ts
+++ src/providers/push.ts
@@ -1,8 +1,13 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FCM_SERVICE_ACCOUNT_FILE!, "base64").toString("utf8")
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert(process.env.FCM_SERVICE_ACCOUNT_FILE!),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
+
 export const fcmProvider = admin.messaging();
